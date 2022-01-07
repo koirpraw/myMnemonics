6299fad353f76c0f419b36278b1cd53c8ebb8d5d
Praweg Koirala <praweg@kduoapps.com>
2022-01-07 16:20:21 -0700

    Initial Commit

---
+++ myMnemonics/myMnemonicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  myMnemonicsApp.swift
+//  myMnemonics
+//
+//  Created by Praweg Koirala on 1/7/22.
+//
+
+import SwiftUI
+
+@main
+struct myMnemonicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
